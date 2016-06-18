d06b8d1b94a0d56da983534d6834e3a1c39d77c7
Blake Netzeband <blake.netzeband@gmail.com>
2016-06-18 17:20:42 -0600

    calculations work correctly - need to add watchers for higher-level calcs. need to remove calculation redundencies when adding rev + expenses

--- app.js
+++ app.js
@@ -4,8 +4,8 @@
   const dependencies = [];
 
   angular
-    .module('roi', dependencies)
-    .controller('MainCtrl', MainCtrl);
+  .module('roi', dependencies)
+  .controller('MainCtrl', MainCtrl);
 
   MainCtrl.$inject = []
   function MainCtrl() {
@@ -14,12 +14,37 @@
     vm.revenueItems = [1,2,3];
     vm.expenseItems = [1,2];
 
+    vm.revenueSingle = 0;
+    vm.revenueMonthly = 0;
+    vm.revenueTotal = 0;
+
+    vm.expenseSingle = 0;
+    vm.expenseMonthly = 0;
+    vm.expenseTotal = 0;
+
+    vm.profitsMonthly = vm.revenueMonthly - vm.expenseMonthly;
+    vm.profitsTotal = vm.revenueTotal - vm.expenseTotal;
+
+    vm.margin = vm.profitsTotal / vm.revenueTotal;
+    vm.roi = (vm.expenseSingle - vm.revenueSingle) / vm.profitsMonthly;
+
     vm.delete = function () {};
 
     vm.addRevenue = function () {
-      console.log(vm.revenue);
+      for (let amount in vm.revenue.single)
+        vm.revenueSingle += +vm.revenue.single[amount];
+      for (let amount in vm.revenue.monthly)
+        vm.revenueMonthly += +vm.revenue.monthly[amount];
+      vm.revenueTotal = vm.revenueSingle + vm.revenueMonthly;
     };
-    vm.addExpenses = function () {};
-  }
 
+    vm.addExpenses = function () {
+      for (let amount in vm.expense.single)
+        vm.expenseSingle += +vm.expense.single[amount];
+      for (let amount in vm.expense.monthly)
+        vm.expenseMonthly += +vm.expense.monthly[amount];
+      vm.expenseTotal = vm.expenseSingle + vm.expenseMonthly;
+    };
+
+  }
 })()
