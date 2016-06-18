8cd28d575aa621483041704cf2f4a91ff53fe11c
Blake Netzeband <blake.netzeband@gmail.com>
2016-06-18 14:51:57 -0600

    added controlleras syntax. added skeleton for all functions. replaced delete button w/ glyphicon. utilized ng-repeat for all inputs

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 'use strict';
-(function () {
-  const dependencies = []
 
-  angular.module('roi', dependencies);
+(function () {
+  const dependencies = [];
+
+  angular
+    .module('roi', dependencies)
+    .controller('MainCtrl', MainCtrl);
+
+  MainCtrl.$inject = ['$scope']
+  function MainCtrl($scope) {
+    let vm = this;
+
+    vm.items = [1,2,3];
+    vm.expenses = [1,2];
+
+    vm.delete = function () {};
+
+    vm.addRevenue = function () {};
+    vm.addExpenses = function () {};
+  }
 
 })()
