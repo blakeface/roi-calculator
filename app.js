2a7bcd033a2ab79dca95f763f90e2da95396c068
Blake Netzeband <blake.netzeband@gmail.com>
2016-06-20 19:46:49 -0600

    removed id key from items arrays. now lists within dom by . form works correctly for revenue

--- app.js
+++ app.js
@@ -11,8 +11,8 @@
   function MainCtrl($scope) {
     let vm = this;
 
-    vm.revenueItems = [1];
-    vm.expenseItems = [1];
+    vm.revenueItems = [{single: '400', monthly: '300'}];
+    vm.expenseItems = [{single: '150', monthly: '75'}];
 
     vm.revenueSingle = 0;
     vm.revenueMonthly = 0;
@@ -28,19 +28,22 @@
     vm.margin = vm.profitsTotal / vm.revenueTotal;
     vm.roi = (vm.expenseSingle - vm.revenueSingle) / vm.profitsMonthly;
 
-    vm.addRevenueItem = function () {
-      vm.revenueItems.push(vm.revenueItems[vm.revenueItems.length - 1] + 1)
+    vm.addRevenue = function () {
+      let newRevenue = {};
+      newRevenue.single = $scope.newRevenue.single;
+      newRevenue.monthly = $scope.newRevenue.monthly;
+      vm.revenueItems.push(newRevenue);
     }
     vm.addExpenseItem = function () {
-      vm.expenseItems.push(vm.expenseItems[vm.expenseItems.length - 1] + 1)
+      vm.expenseItems.push(vm.expenseItems.length + 1)
     }
     vm.removeRevenueItem = function (i) {
       if (vm.revenueItems.length > 1)
-        vm.revenueItems.splice(i, 1);
+      vm.revenueItems.splice(i, 1);
     }
     vm.removeExpenseItem = function (i) {
       if (vm.expenseItems.length > 1)
-        vm.expenseItems.splice(i, 1);
+      vm.expenseItems.splice(i, 1);
     }
 
     $scope.$watch(function(){
