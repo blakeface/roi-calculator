f4ef012ce3368368faf5c43e8fb2f5fcf007a1ef
Blake Netzeband <blake.netzeband@gmail.com>
2016-06-21 12:53:51 -0600

    refactored calculation fn to reset each variable to 0

--- app.js
+++ app.js
@@ -28,39 +28,36 @@
       {single: '103.22', monthly: '105'},
     ];
 
-    vm.addRevenueItem = function () {
-      let newRevenue = {};
+    vm.addRevenueItem = function (argument) {
+      let newRevenue = {single: 0, monthly: 0};
       if ($scope.newRevenue.single) newRevenue.single = $scope.newRevenue.single;
-      else newRevenue.single = 0;
-
       if ($scope.newRevenue.monthly) newRevenue.monthly = $scope.newRevenue.monthly;
-      else newRevenue.monthly = 0;
-
       vm.revenueItems.push(newRevenue);
       $scope.newRevenue = {};
-      revenueCalculations();
+      revenueCalculation();
     }
     vm.addExpenseItem = function () {
-      let newExpense = {};
+      let newExpense = {single: 0, monthly: 0};
       if ($scope.newExpense.single) newExpense.single = $scope.newExpense.single;
-      else newExpense.single = 0;
       if ($scope.newExpense.monthly) newExpense.monthly = $scope.newExpense.monthly;
-      else newExpense.monthly = 0;
       vm.expenseItems.push(newExpense);
       $scope.newExpense = {};
-      expenseCalculations();
+      expenseCalculation();
     }
     vm.removeRevenueItem = function (i) {
       vm.revenueItems.splice(i, 1);
+      revenueCalculation();
     }
     vm.removeExpenseItem = function (i) {
       vm.expenseItems.splice(i, 1);
+      expenseCalculation();
     }
 
-    revenueCalculations();
-    expenseCalculations();
+    revenueCalculation();
+    expenseCalculation();
 
-    function revenueCalculations() {
+    function revenueCalculation() {
+      vm.revenueSingle = vm.revenueMonthly = vm.revenueTotal = 0;
       for (let i = 0; i < vm.revenueItems.length; i++) {
         vm.revenueSingle += +vm.revenueItems[i].single;
       }
@@ -68,13 +65,10 @@
         vm.revenueMonthly += +vm.revenueItems[i].monthly;
       }
       vm.revenueTotal = vm.revenueSingle + vm.revenueMonthly;
-      vm.profitsMonthly = vm.revenueMonthly - vm.expenseMonthly;
-      vm.profitsTotal = vm.revenueTotal - vm.expenseTotal;
-      vm.margin = vm.profitsTotal / vm.revenueTotal;
-      vm.roi = (vm.expenseSingle - vm.revenueSingle) / vm.profitsMonthly;
+      profitCalculation();
     };
-
-    function expenseCalculations() {
+    function expenseCalculation() {
+      vm.expenseSingle = vm.expenseMonthly = vm.expenseTotal = 0;
       for (let i = 0; i < vm.expenseItems.length; i++) {
         vm.expenseSingle += +vm.expenseItems[i].single;
       }
@@ -82,10 +76,13 @@
         vm.expenseMonthly += +vm.expenseItems[i].monthly;
       }
       vm.expenseTotal = vm.expenseSingle + vm.expenseMonthly;
-      vm.profitsMonthly = vm.revenueMonthly - vm.expenseMonthly;
+      profitCalculation();
+    };
+    function profitCalculation() {
+g      vm.profitsMonthly = vm.revenueMonthly - vm.expenseMonthly;
       vm.profitsTotal = vm.revenueTotal - vm.expenseTotal;
       vm.margin = vm.profitsTotal / vm.revenueTotal;
       vm.roi = (vm.expenseSingle - vm.revenueSingle) / vm.profitsMonthly;
-    };
+    }
   }
 })()
